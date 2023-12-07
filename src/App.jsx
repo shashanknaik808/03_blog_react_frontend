7d19e1d53a4d2ccc10b4bf5e256313ede120c25d
Shashank Naik <shashanknaik808@gmail.com>
2023-12-07 13:11:24 +0530

    Reorganized App component and implemented React Router for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddBlog from './components/AddBlog';
+import Auth from './components/Auth';
+import BlogDetail from './components/BlogDetail';
 import Header from './components/Header';
+import UserBlogs from './components/UserBlogs';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <React.Fragment >
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetail />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 
