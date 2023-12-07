7e8fc5d725e1544ab984895732e5e83d8ed477be
Shashank Naik <shashanknaik808@gmail.com>
2023-12-07 12:48:22 +0530

    Created Header Component with AppBar, Toolbar & Styled Buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
+import {
+    AppBar,
+    Box,
+    Button,
+    Toolbar,
+    Typography
+} from '@mui/material';
 import React from 'react';
 
 function Header() {
     return (
-        <div>Header</div>
+        <div>
+            <AppBar sx={{ background: 'linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(3,2,51,1) 6%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);' }}>
+                <Toolbar>
+                    <Typography variant='h4'>
+                        Blog App
+                    </Typography>
+                    <Box display={"flex"} marginLeft={"auto"}>
+                        <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+                        <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </div>
     )
 }
 
