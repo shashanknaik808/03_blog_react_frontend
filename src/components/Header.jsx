45c829c82b331ede94f105ef93609b7e550d4bc5
Shashank Naik <shashanknaik808@gmail.com>
2023-12-07 14:51:18 +0530

    fix: Initialize 'value' state in Header component and update Link import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
     AppBar,
     Box,
@@ -5,41 +7,72 @@ import {
     Tab,
     Tabs,
     Toolbar,
-    Typography
+    Typography,
 } from '@mui/material';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 function Header(props) {
-
     const [value, setValue] = useState();
 
     return (
         <div>
-            <AppBar position="sticky" sx={{ background: 'linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(3,2,51,1) 6%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);' }}>
+            <AppBar
+                position="sticky"
+                sx={{
+                    background:
+                        'linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(3,2,51,1) 6%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)',
+                }}
+            >
                 <Toolbar>
-                    <Typography variant='h4'>
-                        Blog App
-                    </Typography>
+                    <Typography variant="h4">Blog App</Typography>
 
-                    {props.isLoggedIn &&
-                        <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
-                            <Tabs textColor='inherite' value={value} onChange={(e, val) => setValue(val)}>
+                    {props.isLoggedIn && (
+                        <Box display={'flex'} marginLeft={'auto'} marginRight={'auto'}>
+                            <Tabs
+                                textColor="inherite"
+                                value={value}
+                                onChange={(e, val) => setValue(val)}
+                            >
                                 <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
                                 <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
                             </Tabs>
-                        </Box>}
+                        </Box>
+                    )}
 
-                    <Box display={"flex"} marginLeft={"auto"}>
-                        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
-                        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
-                        {props.isLoggedIn &&
-                            <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>}
+                    <Box display={'flex'} marginLeft={'auto'}>
+                        <Button
+                            LinkComponent={Link}
+                            to="/auth"
+                            variant="contained"
+                            sx={{ margin: 1, borderRadius: 10 }}
+                            color="warning"
+                        >
+                            Login
+                        </Button>
+                        <Button
+                            LinkComponent={Link}
+                            to="/auth"
+                            variant="contained"
+                            sx={{ margin: 1, borderRadius: 10 }}
+                            color="warning"
+                        >
+                            Signup
+                        </Button>
+                        {props.isLoggedIn && (
+                            <Button
+                                LinkComponent={Link}
+                                to="/auth"
+                                variant="contained"
+                                sx={{ margin: 1, borderRadius: 10 }}
+                                color="warning"
+                            >
+                                Logout
+                            </Button>
+                        )}
                     </Box>
                 </Toolbar>
             </AppBar>
         </div>
-    )
+    );
 }
 
 export default Header;
