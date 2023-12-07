01335e7ff7aa139c4f2824491a39fcefb6a598d6
Shashank Naik <shashanknaik808@gmail.com>
2023-12-07 13:27:17 +0530

    Implemented state for navigation tabs and updated styling for authentication buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ import {
     AppBar,
     Box,
     Button,
+    Tab,
+    Tabs,
     Toolbar,
     Typography
 } from '@mui/material';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
+
+    const [value, setValue] = useState();
+
     return (
         <div>
             <AppBar position="sticky" sx={{ background: 'linear-gradient(324deg, rgba(2,0,36,1) 0%, rgba(3,2,51,1) 6%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);' }}>
@@ -15,9 +21,18 @@ function Header() {
                     <Typography variant='h4'>
                         Blog App
                     </Typography>
+
+                    <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
+                        <Tabs textColor='inherite' value={value} onChange={(e, val) => setValue(val)}>
+                            <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+                            <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+                        </Tabs>
+                    </Box>
+
                     <Box display={"flex"} marginLeft={"auto"}>
-                        <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
-                        <Button variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+                        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
+                        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
+                        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>
                     </Box>
                 </Toolbar>
             </AppBar>
