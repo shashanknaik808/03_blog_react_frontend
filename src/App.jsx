b7530623c4abed549576302314ca5ab0ca71def6
Shashank Naik <shashanknaik808@gmail.com>
2023-12-07 14:41:30 +0530

    Conditional rendering for navigation tabs based on isLoggedIn prop

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Auth from './components/Auth';
 import BlogDetail from './components/BlogDetail';
 import Header from './components/Header';
 import UserBlogs from './components/UserBlogs';
-import React from 'react';
+import React, { useState } from 'react';
 import Blogs from './components/Blogs';
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ import {
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
 
     const [value, setValue] = useState();
 
@@ -22,12 +22,12 @@ function Header() {
                         Blog App
                     </Typography>
 
-                    <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
+                    {props.isLoggedIn && <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
                         <Tabs textColor='inherite' value={value} onChange={(e, val) => setValue(val)}>
                             <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
                             <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
                         </Tabs>
-                    </Box>
+                    </Box>}
 
                     <Box display={"flex"} marginLeft={"auto"}>
                         <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
