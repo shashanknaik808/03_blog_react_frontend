243b6ed25ae9fb922c8c8035b47e443e0b8a893f
Shashank Naik <shashanknaik808@gmail.com>
2023-12-07 16:47:38 +0530

    Handle errors in API request with appropriate alerts and set login status accordingly

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -12,66 +12,65 @@ function Auth() {
     const [isSignup, setIsSignup] = useState(false);
 
     function handleChange(e) {
-        setInputs(function (prevState) {
-            return {
-                ...prevState,
-                [e.target.name]: e.target.value
-            };
-        });
-    }
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        })
+        );
+    };
 
     async function sendRequest(type) {
         const res = await axios.post(`http://localhost:5000/api/user/${type}`, {
             name: inputs.name,
             email: inputs.email,
             password: inputs.password
-        }).catch(err => console.log(err))
-        console.log(res);
-        let data = null;
-        if (res) {
-            data = await res.data;
-            console.log(data);
+        }).catch(err => {
+            if (err.response.request.status === 404) {
+                alert("User does not exist");
+                props.setIsLoggedIn(false);
+            } else if (err.response.request.status === 400) {
+                alert("Invalid password");
+                props.setIsLoggedIn(true);
+            }
+        })
+
+        function handleSubmit(e) {
+            e.preventDefault();
+            console.log(inputs);
+            if (isSignup) {
+                sendRequest("signup").then(data => console.log(data))
+            } else {
+                sendRequest("login").then(data => console.log(data))
+            }
         }
-        return data;
+
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <Box
+                        maxWidth={400}
+                        display={"flex"}
+                        flexDirection={"column"}
+                        alignItems={"center"}
+                        justifyContent={"center"}
+                        boxShadow={"10px 10px 20px #ccc"}
+                        padding={3}
+                        margin={"auto"}
+                        marginTop={5}
+                        borderRadius={5}
+                    >
+                        <Typography variant='h2' padding={3} textAlign={"center"}>{isSignup ? "Signup" : "Login"}</Typography>
+
+                        {isSignup &&
+                            <TextField name='name' onChange={handleChange} value={inputs.name} type='name' placeholder='Name' margin='normal' />}{" "}
+                        <TextField name='email' onChange={handleChange} type='email' value={inputs.email} placeholder='Email' margin='normal' />
+                        <TextField name='password' onChange={handleChange} type='password' value={inputs.password} placeholder='Password' margin='normal' />
+                        <Button type='submit' variant='contained' sx={{ borderRadius: 3, marginTop: 3 }} color='warning'>Submit</Button>
+                        <Button onClick={() => setIsSignup(!isSignup)} sx={{ borderRadius: 3, marginTop: 3 }}>Change to {isSignup ? "Login" : "Signup"}</Button>
+                    </Box>
+                </form>
+            </div>
+        );
     }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(inputs);
-        if (isSignup) {
-            sendRequest("signup").then(data => console.log(data))
-        } else {
-            sendRequest("login").then(data => console.log(data))
-        }
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <Box
-                    maxWidth={400}
-                    display={"flex"}
-                    flexDirection={"column"}
-                    alignItems={"center"}
-                    justifyContent={"center"}
-                    boxShadow={"10px 10px 20px #ccc"}
-                    padding={3}
-                    margin={"auto"}
-                    marginTop={5}
-                    borderRadius={5}
-                >
-                    <Typography variant='h2' padding={3} textAlign={"center"}>{isSignup ? "Signup" : "Login"}</Typography>
-
-                    {isSignup &&
-                        <TextField name='name' onChange={handleChange} value={inputs.name} type='name' placeholder='Name' margin='normal' />}{" "}
-                    <TextField name='email' onChange={handleChange} type='email' value={inputs.email} placeholder='Email' margin='normal' />
-                    <TextField name='password' onChange={handleChange} type='password' value={inputs.password} placeholder='Password' margin='normal' />
-                    <Button type='submit' variant='contained' sx={{ borderRadius: 3, marginTop: 3 }} color='warning'>Submit</Button>
-                    <Button onClick={() => setIsSignup(!isSignup)} sx={{ borderRadius: 3, marginTop: 3 }}>Change to {isSignup ? "Login" : "Signup"}</Button>
-                </Box>
-            </form>
-        </div>
-    );
-}
-
-export default Auth;
+    export default Auth;
