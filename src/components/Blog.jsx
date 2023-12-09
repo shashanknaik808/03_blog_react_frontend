a18dad8ca405461a14a7bd194246aecb2b1bc3c3
Shashank Naik <shashanknaik808@gmail.com>
2023-12-10 00:04:30 +0530

    Imported and used MUI components (Card, CardHeader, CardMedia, CardContent, Avatar, IconButton, Typography) and icons (MoreVert, Edit, DeleteForever) in a React component, also included Box and useNavigate from react-router-dom

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,17 @@
-import { Avatar, Card, CardContent, CardHeader, CardMedia, Typography } from '@mui/material'
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Box from '@mui/material/Box';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useNavigate } from 'react-router-dom';
 
 function Blog(props) {
     return (
