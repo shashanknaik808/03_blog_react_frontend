6f26725502ffd0c3184ef837d678e725d8fff9aa
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 23:35:18 +0530

    Added prop references in Blog component for userName, title, imageURL, and description

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,30 +1,35 @@
 import { Avatar, Card, CardContent, CardHeader, CardMedia, Typography } from '@mui/material'
 import React from 'react';
 
-function Blog() {
+function Blog(props) {
     return (
         <div>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{
+                width: "40%",
+                margin: 'auto',
+                mt: 2,
+                paddind: 2,
+                boxShadow: "5px 5px 10px #ccc",
+                ":hover": { boxShadow: "10px 10px 20px #ccc" }
+            }}>
                 <CardHeader
                     avatar={
                         <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
-                            R
+                            {props.userName}
                         </Avatar>
                     }
-                    title="Shrimp and Chorizo Paella"
+                    title={props.title}
                     subheader="September 14, 2016"
                 />
                 <CardMedia
                     component="img"
                     height="194"
-                    image="/static/images/cards/paella.jpg"
+                    image={props.imageURL}
                     alt="Paella dish"
                 />
                 <CardContent>
                     <Typography variant="body2" color="text.secondary">
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
+                        {props.description}
                     </Typography>
                 </CardContent>
             </Card>
