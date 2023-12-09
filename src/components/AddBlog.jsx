489809ebf5a6b6ba47b67a2db99574fa9be5118a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-10 00:02:30 +0530

    Implemented state and functions for handling form inputs, sending a request to add a blog, and logging the input data and response

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,6 +1,40 @@
-import { Box, InputLabel, TextField, Typography } from '@mui/material'
+import React, { useState } from 'react';
+import { Box, Button, Typography, InputLabel, TextField } from '@mui/material';
+import axios from 'axios';
+
+const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
 
 function AddBlog() {
+
+    const [inputs, setInputs] = useState({
+        title: "", description: "", imageURL: ""
+    })
+
+    function handleChange(e) {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        })
+        );
+    }
+
+    async function sendRequest() {
+        const res = await axios.post(`http://localhost:5000/api/blog/add`, {
+            title: inputs.title,
+            description: inputs.description,
+            image: inputs.imageURL,
+            user: localStorage.getItem("userID")
+        }).catch(err => console.log(err))
+        const data = await res.data;
+        return data;
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(inputs);
+        sendRequest().then(data => console.log(data))
+    }
+
     return (
         <div>
             <form>
