ed20268ec85af05a474f91bae06b8353db5cccff
Shashank Naik <shashanknaik808@gmail.com>
2023-12-10 00:08:24 +0530

    Conditionally mapped over 'blogs' array and rendered 'Blog' component with user-specific data

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,34 +1,37 @@
+import React, { useEffect } from 'react'
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Blog from './Blog';
+import { useState } from 'react';
+import Blog from './Blog.jsx';
 
 function Blogs() {
 
     const [blogs, setBlogs] = useState([]);
 
     async function sendRequest() {
-        const res = await axios.get("http://localhost:5000/api/blog")
-            .catch(err => console.log(err));
+        const res = await axios
+            .get(`http://localhost:5000/api/blog`)
+            .catch(err => console.log(err))
         const data = await res.data;
         return data;
     };
 
     useEffect(() => {
-        sendRequest().then((data) => console.log(data));  // Correct: logs the data received from sendRequest
+        sendRequest()
+            .then(data => setBlogs(data.blogs));
     }, []);
 
 
     return (
         <div>
-            {(blogs.length !== 0) &&
-                blogs.map((blog, index) => (
-                    <Blog
-                        userName={blog.user.name}
-                        description={blog.description}
-                        imageURL={blog.image}
-                        title={blog.title} />
-                ))
-            }
+            {(blogs.length != 0) && blogs.map((blog, index) =>
+                <Blog
+                    id={blog._id}
+                    isUser={localStorage.getItem("userID") === blog.user._id}
+                    user={blog.user.name}
+                    description={blog.description}
+                    imageURL={blog.image}
+                    title={blog.title} />
+            )}
         </div>
     )
 }
