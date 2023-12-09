519e31ef88ca344897e4d9aba40e993161435d75
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 19:28:34 +0530

    Implemented asynchronous function 'sendRequest' using axios to fetch blog data

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,8 +1,16 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function Blogs() {
 
     const [blogs, setBlogs] = useState([]);
+
+    async function sendRequest() {
+        const res = await axios.get("http://localhost:5000/api/blog")
+            .catch(err => console.log(err));
+        const data = await res.data;
+        return data;
+    };
 
     return (
         <div>Blogs</div>
