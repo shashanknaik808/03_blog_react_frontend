6c20da4fc137be5e9df1fcb78ce854250e2fd154
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 23:43:04 +0530

    Initialized 'blogs' state variable with an empty array and retrieved 'userID' from local storage

--- src/components/UserBlogs.jsx
+++ src/components/UserBlogs.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function UserBlogs() {
+    const [blogs, setBlogs] = useState([]);
+    const id = localStorage.getItem("userID");
+
     return (
         <div>UserBlogs</div>
     )
