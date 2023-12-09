af5088f8ad2480910f2dea72da6129deaaecae1c
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 23:58:50 +0530

    Added a form with input fields for title, description, and imageURL in a MUI Box component

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
-import { Box, Button, Typography, InputLabel, TextField } from '@mui/material';
-import axios from 'axios';
-
-const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
+import { Box, InputLabel, TextField, Typography } from '@mui/material'
 
 function AddBlog() {
     return (
-        <div>AddBlog</div>
+        <div>
+            <form>
+                <Box>
+                    <Typography>Post your Blog</Typography>
+                    <InputLabel>Title</InputLabel>
+                    <TextField />
+                    <InputLabel>Description</InputLabel>
+                    <TextField />
+                    <InputLabel>ImageURL</InputLabel>
+                    <TextField />
+                </Box>
+            </form>
+        </div>
     )
 }
 
