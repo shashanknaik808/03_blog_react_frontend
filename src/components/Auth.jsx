3eed2ed6105d0251840ee91ef4c6c0e683f4919a
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 23:25:37 +0530

    Implemented user authentication logic using axios, with error handling for invalid passwords and non-existent users

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,59 +1,58 @@
-import React, { useState } from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material'
 import axios from 'axios';
-import { useNavigate } from 'react-router';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Auth(props) {
     const navigate = useNavigate();
     const [inputs, setInputs] = useState({
-        name: "",
-        email: "",
-        password: ""
-    });
-
+        name: "", email: "", password: ""
+    })
     const [isSignup, setIsSignup] = useState(false);
 
     function handleChange(e) {
-        setInputs((prevState) => ({
+        setInputs(prevState => ({
             ...prevState,
             [e.target.name]: e.target.value
         }));
     }
 
     async function sendRequest(type) {
-        try {
-            const res = await axios.post(`http://localhost:5000/api/user/${type}`, {
-                name: inputs.name,
-                email: inputs.email,
-                password: inputs.password
-            });
-
-            console.log(res.data);
-        } catch (err) {
+        const res = await axios.post(`http://localhost:5000/api/user/${type}`, {
+            name: inputs.name,
+            email: inputs.email,
+            password: inputs.password
+        }).catch(err => {
             if (err.response.request.status === 404) {
-                alert("User does not exist");
+                alert("User DOes not exist");
                 props.setIsLoggedIn(false);
-            } else if (err.response.request.status === 400) {
-                alert("Invalid password");
-                props.setIsLoggedIn(true);
             }
-        }
+            else if (err.response.request.status === 400) {
+                alert("Invalid Password");
+                props.setIsLoggedIn(false);
+            }
+        });
+
+        let data = await res.data;
+        return data;
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(inputs);
         if (isSignup) {
             sendRequest("signup")
-                .then(data => console.log(data))
-                .catch(err => console.error("Error in signup", err));
+                .then(data => navigate("/auth"))
+                .catch(err => "There is some mistake in signup");
         } else {
-            sendRequest("login")
-                .then(data => localStorage.setItem("userID", data.user._id))
-                .then(data => console.log(data))
-                .catch(err => console.error("Error in login", err));
+            sendRequest('login')
+                .then(data => {
+                    props.setIsLoggedIn(true);
+                    navigate("/blogs")
+                })
+                .catch(err => "there is some mistake in login");
         }
     }
+
 
 
     return (
@@ -62,28 +61,29 @@ function Auth(props) {
                 <Box
                     maxWidth={400}
                     display={"flex"}
-                    flexDirection={"column"}
-                    alignItems={"center"}
-                    justifyContent={"center"}
-                    boxShadow={"10px 10px 20px #ccc"}
+                    alignItems={'center'}
+                    flexDirection={'column'}
+                    justifyContent={'center'}
+                    boxShadow={'10px 10px 20px #ccc'}
                     padding={3}
-                    margin={"auto"}
+                    margin={'auto'}
                     marginTop={5}
-                    borderRadius={5}
-                >
-                    <Typography variant='h2' padding={3} textAlign={"center"}>{isSignup ? "Signup" : "Login"}</Typography>
+                    borderRadius={5}>
+                    <Typography variant='h2' padding={3} textAlign={'center'}>
+                        {isSignup ? "Sign Up" : "Login"}
+                    </Typography>
 
-                    {isSignup &&
-                        <TextField name='name' onChange={handleChange} value={inputs.name} type='name' placeholder='Name' margin='normal' />}{" "}
-                    <TextField name='email' onChange={handleChange} type='email' value={inputs.email} placeholder='Email' margin='normal' />
-                    <TextField name='password' onChange={handleChange} type='password' value={inputs.password} placeholder='Password' margin='normal' />
+                    {isSignup && <TextField name='name' onChange={handleChange} value={inputs.name} placeholder='Name' margin='normal' />}
+                    <TextField name='email' onChange={handleChange} value={inputs.email} type={'email'} placeholder='Email' margin='normal' />
+                    <TextField name='password' onChange={handleChange} value={inputs.password} type={'password'} placeholder='Password' margin='normal' />
                     <Button type='submit' variant='contained' sx={{ borderRadius: 3, marginTop: 3 }} color='warning'>Submit</Button>
-                    <Button onClick={() => setIsSignup(!isSignup)} sx={{ borderRadius: 3, marginTop: 3 }}>Change to {isSignup ? "Login" : "Signup"}</Button>
+                    <Button onClick={() => setIsSignup(!isSignup)} sx={{ borderRadius: 3, marginTop: 3 }}>
+                        Change To {isSignup ? "Login" : "Signup"}
+                    </Button>
                 </Box>
             </form>
         </div>
-    );
+    )
 }
-
 
 export default Auth;
