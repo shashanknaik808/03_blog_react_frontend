c3923c99302a04493f448a07973cc2ba220b53a6
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 23:43:43 +0530

    Implemented asynchronous function 'sendRequest' using axios to fetch user-specific blog data

--- src/components/UserBlogs.jsx
+++ src/components/UserBlogs.jsx
@@ -4,6 +4,13 @@ function UserBlogs() {
     const [blogs, setBlogs] = useState([]);
     const id = localStorage.getItem("userID");
 
+    async function sendRequest() {
+        const res = await axios.get(`http://localhost:5000/api/blog/user/${id}`)
+            .catch(err => console.log(err))
+        const data = await res.data;
+        return data;
+    }
+
     return (
         <div>UserBlogs</div>
     )
