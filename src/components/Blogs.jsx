9a2726d1e17e8d013256f8d7b8ea0ac0cfe9cc3d
Shashank Naik <shashanknaik808@gmail.com>
2023-12-10 00:16:57 +0530

    Added unique 'key' prop to child components in Blogs.jsx to resolve React warning

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,37 +1,34 @@
-import React, { useEffect } from 'react'
 import axios from 'axios';
-import { useState } from 'react';
-import Blog from './Blog.jsx';
+import React, { useEffect, useState } from 'react';
+import Blog from './Blog';
 
 function Blogs() {
-
     const [blogs, setBlogs] = useState([]);
 
     async function sendRequest() {
-        const res = await axios
-            .get(`http://localhost:5000/api/blog`)
-            .catch(err => console.log(err))
+        const res = await axios.get("http://localhost:5000/api/blog")
+            .catch(err => console.log(err));
         const data = await res.data;
         return data;
     };
 
     useEffect(() => {
-        sendRequest()
-            .then(data => setBlogs(data.blogs));
+        sendRequest().then((data) => setBlogs(data.blogs));
     }, []);
-
 
     return (
         <div>
-            {(blogs.length != 0) && blogs.map((blog, index) =>
-                <Blog
-                    id={blog._id}
-                    isUser={localStorage.getItem("userID") === blog.user._id}
-                    user={blog.user.name}
-                    description={blog.description}
-                    imageURL={blog.image}
-                    title={blog.title} />
-            )}
+            {blogs.length !== 0 &&
+                blogs.map((blog, index) => (
+                    <Blog
+                        key={index} 
+                        userName={blog.user.name}
+                        description={blog.description}
+                        imageURL={blog.image}
+                        title={blog.title}
+                    />
+                ))
+            }
         </div>
     )
 }
