9324088986f236e4b82fae270fd2b2784012e93e
Shashank Naik <shashanknaik808@gmail.com>
2023-12-10 00:03:20 +0530

    Created a styled form with MUI components for adding a blog, including input fields for title, description, and image

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -37,15 +37,31 @@ function AddBlog() {
 
     return (
         <div>
-            <form>
-                <Box>
-                    <Typography>Post your Blog</Typography>
-                    <InputLabel>Title</InputLabel>
-                    <TextField />
-                    <InputLabel>Description</InputLabel>
-                    <TextField />
-                    <InputLabel>ImageURL</InputLabel>
-                    <TextField />
+            <form onSubmit={handleSubmit}>
+                <Box
+                    border={3}
+                    borderColor='linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,19,121,1) 0%, rgba(0,212,255,1) 100%)'
+                    borderRadius={10}
+                    boxShadow="10px 10px 20px #ccc"
+                    padding={3}
+                    margin={"auto"}
+                    marginTop={3}
+                    display='flex'
+                    flexDirection={'column'}
+                    width={"80%"}
+                >
+                    <Typography fontWeight={'bold'}
+                        padding={3}
+                        color="grey"
+                        variant='h2'
+                        textAlign='center'>Post Your Blog</Typography>
+                    <InputLabel sx={labelStyle}>Title</InputLabel>
+                    <TextField name='title' onChange={handleChange} value={inputs.title} margin='auto' variant='outlined' />
+                    <InputLabel sx={labelStyle}>Description</InputLabel>
+                    <TextField name='description' onChange={handleChange} value={inputs.description} margin='auto' variant='outlined' />
+                    <InputLabel sx={labelStyle}>Image</InputLabel>
+                    <TextField name='imageURL' onChange={handleChange} value={inputs.imageURL} margin='auto' variant='outlined' />
+                    <Button type="submit" sx={{ mt: 2, borderRadius: 4 }} variant='contained' color='warning'>Submit</Button>
                 </Box>
             </form>
         </div>
