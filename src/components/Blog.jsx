e2e7dfa85d76a4917ce924a95f73f014adff7dce
Shashank Naik <shashanknaik808@gmail.com>
2023-12-09 19:36:15 +0530

    Created a Card component with Avatar and CardHeader in MUI, displaying a sample recipe

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Avatar, Card, CardHeader } from '@mui/material'
+import React from 'react';
 
 function Blog() {
     return (
-        <div>Blog</div>
+        <div>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: 'red' }} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+            </Card>
+        </div>
     )
 }
 
-export default Blog
+export default Blog;
